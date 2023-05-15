e08e32c66a53be239b0f446d9e4b5d4efd772de2
mahdee18 <mahdeec10@gmail.com>
2023-05-15 18:29:45 +0600

    Post Service item

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3222
 
 // Middleware
-app.use = (cors())
-app.express = (express.json())
+app.use(cors())
+app.use(express.json())
 
 
 
@@ -28,10 +28,42 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const serviceCollection = client.db("carDoctor").collection('services')
+        const serviceCollection = client.db("carDoctor").collection('services');
+
+        const serviceBookCollection = client.db("carDoctor").collection('servicesBooks')
+
+
         app.get('/services', async(req,res)=>{
             const cursor = serviceCollection.find()
             const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query= {_id: new ObjectId(id)}
+            const option = {
+                projection: {title:1, price:1,service_id:1,img:1,}
+            }
+            const result = await serviceCollection.findOne(query,option)
+            res.send(result)
+        })
+
+        // Services book
+
+        app.get('/servicesBook',async(req,res)=>{
+            let query = {}
+            if(req.query?.email){
+                query= {email: req.query.email}
+            }
+
+            const result = await serviceBookCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.post('/servicesBook',async(req,res)=>{
+            const booking = req.body;
+            console.log(booking)
+            const result = await serviceBookCollection.insertOne(booking)
             res.send(result)
         })
 
